e078da56be936550f9d95595ed0365684f853664
karahman <jakarahman007@gmail.com>
2020-09-15 18:43:18 +0700

    add transformer

---
+++ app/Helpers/Transformer.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Helpers;
+
+use Illuminate\Http\JsonResponse;
+
+class Transformer 
+{
+    /**
+     * Meta
+     *
+     * @param   bool    $ok       
+     * @param   string  $message  
+     *
+     * @return  array            
+     */
+    public static function meta(bool $ok, string $message)
+    {
+        return [
+            'ok' => $ok,
+            'message' => $message
+        ];
+    }
+
+    /**
+     * Success response json.
+     *
+     * @param   string  $message  
+     * @param   mixed   $data     
+     * @param   int     $status   
+     * @param   array   $headers
+     *
+     * @return  JsonResponse           
+     */
+    public static function ok(string $message, $data = null, int $status = 200, array $headers = [])
+    {
+        return response()->json(
+            array_merge(
+                self::meta(true, $message, $data),
+                ['data' => $data]
+            ),
+            $status, $headers);
+    }
+
+    /**
+     * Success response json.
+     *
+     * @param   string  $message  
+     * @param   mixed   $data     
+     * @param   int     $status   
+     * @param   array   $headers
+     *
+     * @return  JsonResponse           
+     */
+    public static function fail(string $message, $data = null, int $status = 200, array $headers = [])
+    {
+        return response()->json(
+            array_merge(
+                self::meta(false, $message, $data),
+                ['data' => $data]
+            ),
+            $status, $headers);
+    }
+}
