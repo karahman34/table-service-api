475a380d0f28b535a6d54a797e3d6ae4c9d7dcce
karahman <jakarahman007@gmail.com>
2020-09-23 15:32:19 +0700

    edit the output

--- app/Http/Resources/TransactionsCollection.php
+++ app/Http/Resources/TransactionsCollection.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Resources;
 
+use App\Transaction;
 use Illuminate\Http\Resources\Json\ResourceCollection;
 
 class TransactionsCollection extends ResourceCollection
@@ -14,6 +15,8 @@ class TransactionsCollection extends ResourceCollection
      */
     public function toArray($request)
     {
-        return parent::toArray($request);
+        return $this->resource->map(function (Transaction $transaction) {
+            return new TransactionResource($transaction);
+        });
     }
 }
